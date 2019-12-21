d7d078f7c7ba4a3a7320d9f84e5d85660cd59626
Blaie Angoma <bangoma@apogee.us>
2019-12-21 02:26:41 -0600

    ajout explication

---
+++ triange.py
@@ -0,0 +1,6 @@
+# 1. creer un compte sur github
+# 2. creer un repository
+# 3. installer git et le configurer
+# 4. installer visual studio code
+# 5. installer python3
+# 6. commencer votre projet 
